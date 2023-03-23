f8a1bcbf76bc41fe454906115a313a6ab9881d07
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-03-23 22:17:28 +0100

    created chatInput section

--- src/component/chat/Chat.tsx
+++ src/component/chat/Chat.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Chat.scss";
 import ChatHeader from "./ChatHeader";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
+import GifIcon from "@mui/icons-material/Gif";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 const Chat = () => {
   return (
     <div className="chat">
       <ChatHeader />
-      {/* <div className="chatMessasge"></div>
-      <div className="chatInput"></div> */}
+      {/* <div className="chatMessasge"></div>*/}
+      <div className="chatInput">
+        <AddCircleOutlineIcon />
+        <form>
+          <input type="text" placeholder="Send Message" />
+          <button type="submit" className="chatInputButton">
+            submit
+          </button>
+        </form>
+        <div className="chatInputIcons">
+          <CardGiftcardIcon />
+          <GifIcon />
+          <EmojiEmotionsIcon />
+        </div>
+      </div>
     </div>
   );
 };
