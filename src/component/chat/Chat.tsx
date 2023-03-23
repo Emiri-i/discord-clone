a6c0c10d0feb3606d6b71bd65f7ad4cf37616692
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-03-23 21:27:48 +0100

    created chat component

---
+++ src/component/chat/Chat.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Chat.scss";
+import ChatHeader from "./ChatHeader";
+const Chat = () => {
+  return (
+    <div className="chat">
+      <ChatHeader />
+      {/* <div className="chatMessasge"></div>
+      <div className="chatInput"></div> */}
+    </div>
+  );
+};
+
+export default Chat;
