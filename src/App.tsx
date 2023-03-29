c36567684cb9c846c78e6f22745f7948a67b7690
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-03-29 22:55:17 +0200

    created user slice type to get user login status

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.scss";
 import Sidebar from "./component/sidebar/Sidebar";
 import Chat from "./component/chat/Chat";
+import Login from "./component/login/Login";
+import { useAppSelector } from "./app/hooks";
 function App() {
+  const user = useAppSelector((state) => state.user);
+
   return (
     <div className="App">
-      <Sidebar />
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
     </div>
   );
 }
