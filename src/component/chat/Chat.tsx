6bea4cce86718478fb526fceda9190d56720d664
emiriiskw@gmail.com <emiriiskw@gmail.com>
2023-04-01 22:18:01 +0200

    added functions to get chat message data

--- src/component/chat/Chat.tsx
+++ src/component/chat/Chat.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Chat.scss";
 import ChatHeader from "./ChatHeader";
 import ChatMessage from "./ChatMessage";
@@ -6,20 +6,109 @@ import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import CardGiftcardIcon from "@mui/icons-material/CardGiftcard";
 import GifIcon from "@mui/icons-material/Gif";
 import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
+import { useAppSelector } from "../../app/hooks";
+import {
+  addDoc,
+  collection,
+  CollectionReference,
+  DocumentData,
+  DocumentReference,
+  onSnapshot,
+  serverTimestamp,
+  Timestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+
+interface Messages {
+  timestamp: Timestamp;
+  message: string;
+  user: {
+    uid: string;
+    photo: string;
+    email: string;
+    displayName: string;
+  };
+}
+
 const Chat = () => {
+  const user = useAppSelector((state) => state.user.user);
+  const [inputText, setInputText] = useState<string>("");
+  const [messages, setMessages] = useState<Messages[]>([]);
+  const channelName = useAppSelector((state) => state.channel.channelName);
+  const channelId = useAppSelector((state) => state.channel.channelId);
+
+  useEffect(() => {
+    let collectionRef = collection(
+      db,
+      "channels",
+      String(channelId),
+      "messages"
+    );
+    onSnapshot(collectionRef, (snapshot) => {
+      let results: Messages[] = [];
+      snapshot.docs.forEach((doc) => {
+        results.push({
+          timestamp: doc.data().timestamp,
+          message: doc.data().message,
+          user: doc.data().user,
+        });
+      });
+      setMessages(results);
+    });
+  }, [channelId]);
+
+  const sendMessage = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    //input message info in messages collection of the channels collection
+    const collectionRef: CollectionReference<DocumentData> = collection(
+      db,
+      "channels",
+      String(channelId),
+      "messages"
+    );
+    const docRef: DocumentReference<DocumentData> = await addDoc(
+      collectionRef,
+      {
+        message: inputText,
+        timestamp: serverTimestamp(),
+        user: user,
+      }
+    );
+  };
   return (
     <div className="chat">
-      <ChatHeader />
+      <ChatHeader channelName={channelName} />
       <div className="chatMessasge">
+        {messages.map((message, index) => (
+          <ChatMessage
+            key={index}
+            message={message.message}
+            timestamp={message.timestamp}
+            user={message.user}
+          />
+        ))}
+        {/* <ChatMessage />
         <ChatMessage />
-        <ChatMessage />
-        <ChatMessage />
+        <ChatMessage /> */}
       </div>
       <div className="chatInput">
         <AddCircleOutlineIcon />
         <form>
-          <input type="text" placeholder="Send Message" />
-          <button type="submit" className="chatInputButton">
+          <input
+            type="text"
+            placeholder="Send Message"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInputText(e.target.value)
+            }
+          />
+          <button
+            type="submit"
+            className="chatInputButton"
+            onClick={(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+              sendMessage(e)
+            }>
             submit
           </button>
         </form>
